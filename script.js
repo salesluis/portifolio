f86112ac5cf9b2700bfbb99d73f8583b269c3949
salesluis <salesluis434@gmail.com>
2023-12-12 22:44:07 -0300

    feat: loagind - skils

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const jsClass = document.documentElement.classList.add("js");
 
-function animationSectionScroll(){
+function animationSectionScroll() {
   const sections = document.querySelectorAll(".js-animation-scroll");
-  const windowMetade = window.innerHeight * 0.6
-  
+  const windowMetade = window.innerHeight * 0.6;
+
   function animarScroll() {
     sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isVisdibleSection = (sectionTop - windowMetade) < 0;
-      if (isVisdibleSection) {
+      const isVisibleSection = sectionTop - windowMetade < 0;
+      if (isVisibleSection) {
         section.classList.add("active");
       }
     });
   }
-  animarScroll()
+  animarScroll();
   window.addEventListener("scroll", animarScroll);
 }
-animationSectionScroll()
+animationSectionScroll();
